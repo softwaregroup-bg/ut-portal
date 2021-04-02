ef9dd8319c2f9718e6627fca6155247b601b65bd
Kalin Krustev <kalin.krustev@softwaregroup.com>
2021-04-02 12:52:14 +0300

    docs: theme

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,4 @@
 import { addons } from '@storybook/addons';
-import { themes } from '@storybook/theming';
+import theme from './theme';
 
-addons.setConfig({
-    theme: themes.dark
-});
+addons.setConfig({theme});
