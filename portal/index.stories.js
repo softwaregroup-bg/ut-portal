d9ee416ffe32f573453bb1e0de0fb768a4404a5e
Kalin Krustev <kalin.krustev@gmail.com>
2021-10-22 11:22:41 +0300

    chore: storybook

--- portal/index.stories.js
+++ portal/index.stories.js
@@ -1,40 +1,17 @@
+import React from 'react';
 import {app} from '../storybook';
-import chisel from '../chisel';
-import {tree, treeMock} from './tree';
 
 export default {
-    title: 'Chisel'
+    title: 'Portal'
 };
 
-const organization = [
-    {value: 100, label: 'Africa'},
-    {value: 300, label: 'Asia'},
-    {value: 400, label: 'Australia'},
-    {value: 500, label: 'Europe'},
-    {value: 600, label: 'North America'},
-    {value: 700, label: 'South America'},
-    {value: 101, parents: 100, label: 'Egypt'},
-    {value: 102, parents: 100, label: 'Kenya'},
-    {value: 103, parents: 100, label: 'Ghana'},
-    {value: 104, parents: 100, label: 'Nigeria'},
-    {value: 301, parents: 300, label: 'Philippines'},
-    {value: 302, parents: 300, label: 'India'},
-    {value: 501, parents: 500, label: 'Bulgaria'},
-    {value: 601, parents: 600, label: 'USA'},
-    {value: 701, parents: 700, label: 'Mexico'}
-];
-
 const page = app({
-    implementation: 'microservice',
-    utMicroservice: true,
+    implementation: 'portal',
+    utPortal: true,
     utCore: true
 }, {
-    'customer.organization.graphFetch': () => ({
-        organization: organization.map(({value: id, label: title, ...org}) => ({id, title, ...org}))
-    }),
-    ...chisel(tree()).mock(treeMock)
 }, [
-    function utMicroservice() {
+    function utPortal() {
         return {
             config: () => ({
                 storybook: {
@@ -42,10 +19,19 @@ const page = app({
                 }
             }),
             browser: () => [
-                function component({joi}) {
+                function component() {
                     return [
-                        () => ({ namespace: 'component/microservice' }),
-                        ...chisel(tree(joi)).components()
+                        () => ({
+                            namespace: 'component/portal',
+                            'portal.demo.open': () => ({
+                                title: 'Demo page title',
+                                component: () => {
+                                    return function page() {
+                                        return <div>Demo page body</div>;
+                                    };
+                                }
+                            })
+                        })
                     ];
                 }
             ]
@@ -53,15 +39,4 @@ const page = app({
     }
 ]);
 
-export const TreeBrowse = page('microservice.tree.browse');
-export const TreeOpen = page('microservice.tree.open', 101);
-export const TreeOpenThumbIndex = page('microservice.tree.open', 101, {layout: 'thumbIndex'});
-const treeNew = 'microservice.tree.new';
-export const TreeNew = page(treeNew);
-export const TreeNewConifer = page(treeNew, {type: 'conifer'});
-export const TreeNewBroadleaf = page(treeNew, {type: 'broadleaf'});
-export const TreeNewFlat = page(treeNew, {layout: 'flat'});
-export const TreeNewNested = page(treeNew, {layout: 'nested'});
-export const TreeNew3Col = page(treeNew, {layout: '3col'});
-export const TreeNewThumbIndex = page(treeNew, {layout: 'thumbIndex'});
-export const TreeReport = page('microservice.tree.report', 'list');
+export const TreeBrowse = page('portal.demo.open');
