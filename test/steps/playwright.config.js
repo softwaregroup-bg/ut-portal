35c61ee0160e45a75778d4403acc59764bcfb841
Kalin Krustev <kalin.krustev@gmail.com>
2022-02-23 12:18:00 +0200

    fix: use preinstalled chromium

--- test/steps/playwright.config.js
+++ test/steps/playwright.config.js
@@ -6,6 +6,11 @@ const {resolve} = require('path');
 /** @type {import('@playwright/test').PlaywrightTestConfig<{ username: string, password: string }>} */
 const config = {
     use: {
+        ...process.env.CHROME_BIN && {
+            launchOptions: {
+                executablePath: process.env.CHROME_BIN
+            }
+        },
         baseURL: process.env.UT_URL,
         viewport: { width: 1600, height: 900 },
         username: process.env.UT_USERNAME,
