f5bcf953500d145e55a4aeb739165fd76d901ba1
Jenkins CI <jenkins.ci@softwaregroup-bg.com>
2022-08-25 17:41:44 +0000

    [ci-skip][ci skip] version incremented to 9.2.0-db.0

--- handlers.d.ts
+++ handlers.d.ts
@@ -1,103 +1,130 @@
-import React from 'react';
+declare namespace portalTableTypes {}
+declare namespace db$portal_component_delete$ {
+  export interface params {
+    /**
+     * component ids
+     */
+    componentId?: coreTableTypes.arrayList.params[] | coreTableTypes.arrayList.params;
+    /**
+     * information for the user that makes the operation
+     */
+    meta?: coreTableTypes.metaDataTT.params[] | coreTableTypes.metaDataTT.params;
+  }
+  export type result = any;
+}
+
+declare namespace db$portal_component_edit {
+  export interface params {
+    /**
+     * component to edit
+     */
+    component?: portalTableTypes.componentTT.params[] | portalTableTypes.componentTT.params;
+    /**
+     * information for the user that makes the operation
+     */
+    meta?: coreTableTypes.metaDataTT.params[] | coreTableTypes.metaDataTT.params;
+  }
+  export type result = any;
+}
+
+declare namespace db$portal_component_get {
+  export interface params {
+    componentId?: string | null;
+  }
+  export type result = any;
+}
+
+declare namespace portal_component_delete$ {
+  export interface params {
+    componentId?: string[];
+  }
+  export interface result {
+    component: {
+      componentConfig?: object;
+      componentId: string;
+    }[];
+  }
+}
+
+declare namespace portal_component_edit {
+  export interface params {
+    component?: {
+      componentConfig?: object;
+      componentId: string;
+    };
+  }
+  export interface result {
+    component?: {
+      componentConfig?: object;
+      componentId: string;
+    };
+  }
+}
+
+declare namespace portal_component_get {
+  export interface params {
+    componentId: string;
+  }
+  export interface result {
+    component?: {
+      componentConfig?: object;
+      componentId: string;
+    } | null;
+  }
+}
+
+declare namespace portalTableTypes.componentTT {
+  export interface params {
+    componentConfig?: string | null;
+    componentId?: string | null;
+  }
+  export type result = unknown;
+}
+
+declare namespace portalTableTypes.componentTTU {
+  export interface params {
+    componentConfig?: string | null;
+    componentConfigUpdated?: boolean | null;
+    componentId?: string | null;
+    componentIdUpdated?: boolean | null;
+  }
+  export type result = unknown;
+}
+
 import ut from 'ut-run';
-
-export type dropdownParams =  string[];
-export interface dropdownResult {
-    [name: string]: {
-        value: any,
-        label: string
-    } []
+export interface ports<location = ''> {
+  'db/portal.component.delete'?: ut.handler<db$portal_component_delete$.params, db$portal_component_delete$.result, location>,
+  db$portalComponentDelete?: ut.handler<db$portal_component_delete$.params, db$portal_component_delete$.result, location>,
+  'db/portal.component.edit'?: ut.handler<db$portal_component_edit.params, db$portal_component_edit.result, location>,
+  db$portalComponentEdit?: ut.handler<db$portal_component_edit.params, db$portal_component_edit.result, location>,
+  'db/portal.component.get'?: ut.handler<db$portal_component_get.params, db$portal_component_get.result, location>,
+  db$portalComponentGet?: ut.handler<db$portal_component_get.params, db$portal_component_get.result, location>
 }
+interface methods extends ports {}
 
-interface component {
-    id?: string,
-    title: string,
-    component: remotePage,
-    [name: string]: any
-}
-
-export interface pageDescriptor {
-    title: string,
-    icon?: string,
-    permission?: string | string[],
-    component: (params?: {
-        id?: string,
-        type?: string,
-        layout?: string
-    }) => Promise<React.FC<{id?: string}>> | React.FC<{id?: string}>
-}
-
-export type remotePage = {
-    id: string,
-    title: string,
-    [name: string]: string
-    (params: {}): Promise<pageDescriptor>
-}
-
-interface pages {
-    [name: string]: () => pageDescriptor
-}
-
-export type pageParams = component | remotePage
-export type menuItem = Promise<{
-    title?: string;
-    icon?: string;
-    path: string;
-    params: {
-        id?: string
-    }
-}>
-export type tab = Promise<{
-    title?: string;
-    icon?: string;
-    path: string;
-    Component: React.FC;
-    params: {
-        id?: string
-    }
-}>
-export type action = {
-    title?: string;
-    action: () => {} | void;
-}
 export interface handlers<location = ''> {
-    [name: `component$${string}`]: remotePage,
-    [name: `component/${string}`]: remotePage,
-    handleTabShow?: (tab: remotePage | [remotePage, {id: string}] | {tabs: string, params: any}) => {},
-    'portal.dropdown.list'?: ut.handler<dropdownParams, dropdownResult, location>,
-    portalDropdownList?: ut.handler<dropdownParams, dropdownResult, location>,
-    'portal.menu.item'?: (this: ut.port, page: pageParams, $meta: ut.meta) => menuItem,
-    portalMenuItem?: (this: ut.port | void, page: pageParams) => menuItem,
-    'portal.tab.item'?: (this: ut.port | void, page: pageParams) => tab,
-    portalTabItem?: (this: ut.port | void, page: pageParams) => tab,
-    'portal.menu.help'?: (this: ut.port | void, options: {}) => action,
-    portalMenuHelp?: (this: ut.port | void, options: {}) => action,
+  'portal.component.delete'?: ut.handler<portal_component_delete$.params, portal_component_delete$.result, location>,
+  portalComponentDelete?: ut.handler<portal_component_delete$.params, portal_component_delete$.result, location>,
+  'portal.component.edit'?: ut.handler<portal_component_edit.params, portal_component_edit.result, location>,
+  portalComponentEdit?: ut.handler<portal_component_edit.params, portal_component_edit.result, location>,
+  'portal.component.get'?: ut.handler<portal_component_get.params, portal_component_get.result, location>,
+  portalComponentGet?: ut.handler<portal_component_get.params, portal_component_get.result, location>
 }
 
 export interface errors {
+  'error.portal': ut.error,
+  'error.portal.componentNotFound': ut.errorParam<{ componentId: string | number }>,
+  errorPortalComponentNotFound: ut.errorParam<{ componentId: string | number }>
 }
 
-interface methods {
-    handleDispatchSet?: (dispatch: (action: any) => any) => void
-}
+import core, {coreTableTypes} from 'ut-core/handlers'
+interface methods extends core.handlers {}
 
 export type libFactory = ut.libFactory<methods, errors>
 export type handlerFactory = ut.handlerFactory<methods, errors, handlers<'local'>>
 export type handlerSet = ut.handlerSet<methods, errors, handlers<'local'>>
-export type portalFactory = ut.handlers<handlers, {
-    'portal.reports.get'?: () => Promise<{
+export type test = ut.test<keyof methods | keyof handlers>
 
-    }>
-    'portal.params.get': () => Promise<{
-        theme?: {},
-        portalName: string,
-        portal?: {
-            menu: any,
-            menuClass?: string,
-            tabs: any,
-            hideTabs: boolean
-        }
-    }>
-}>
-export type pageSet<methods, errors> = ut.handlerSet<methods, errors, pages | ut.genericHandlers>
-export type pageFactory<methods, errors> = ut.handlerFactory<methods, errors, pages>
+import portal from 'ut-portal'
+export type pageFactory = portal.pageFactory<methods, errors>
+export type pageSet = portal.pageSet<methods, errors>
