ffd7c7b9b9afbc7c449324b8ea07db8bb5895d83
Kalin Krustev <kalin.krustev@gmail.com>
2022-09-08 17:14:09 +0300

    fix: tests in dark mode by default

--- test/steps/playwright.config.js
+++ test/steps/playwright.config.js
@@ -14,6 +14,7 @@ const config = {
                 executablePath: process.env.CHROME_BIN
             }
         },
+        colorScheme: 'dark',
         baseURL: process.env.UT_URL || rc.url,
         viewport: { width: 1600, height: 900 },
         username: process.env.UT_USERNAME || String(rc.username),
