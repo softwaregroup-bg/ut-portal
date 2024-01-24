337ead7ae69c2e20cfc1deaa370bc589db40de9d
goodlifeinc <genata93@gmail.com>
2024-01-24 11:29:21 +0200

    fix: expose cache methods

--- browser/portal/cache.js
+++ browser/portal/cache.js
@@ -1,14 +1,34 @@
 let cache = {};
-
-/** @type { import("../..").libFactory } */
-module.exports = () => ({
+const lib = {
     setCache(key, value) {
         cache[key] = value;
     },
     getCache(key) {
         return cache[key];
     },
+    deleteCache(key) {
+        delete cache[key];
+    },
     resetCache() {
         cache = {};
+    }
+};
+/** @type { import("../..").libFactory } */
+module.exports = () => ({
+    ...lib,
+    'portal.cache.get': (msg, $meta) => {
+        return lib.getCache(msg.key);
+    },
+    'portal.cache.set': (msg, $meta) => {
+        lib.setCache(msg.key, msg.value);
+        return true;
+    },
+    'portal.cache.delete': (msg, $meta) => {
+        lib.deleteCache(msg.key);
+        return true;
+    },
+    'portal.cache.reset': (msg, $meta) => {
+        lib.resetCache();
+        return true;
     }
 });
